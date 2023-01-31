a60d21e87a55a4c61b9bc1b3509094ed6bf57de7
Ne0re0 <neo.leborgne@gmail.com>
2023-01-31 17:25:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # BlackJack
-Etude personnelle pour determiner quelle est la meilleur stratégie à tenir au jeu du BlackJack
+Après avoir découvert les jeux du casino, mon esprit est resté bloqué sur le jeu du BlackJack (et ce qu'il y avait dans mon porte monnaie aussi...).  
+Comme ce jeu m'intriguait, j'ai fini par me poser des questions. 
+- Pourquoi leur IA s'arrête de piocher à partir de 17 points ?
+- Quel est la meilleur stratégie à suivre pour maximiser ses chances de gagner ?
+Dans un premier temps, j'ai codée le jeu.
+Dans un deuxième temps, j'ai pu établir le pourquoi 17 points ? 
+Mais ça me paraissait trop naif comme possibilité  
+
+On sait que le croupier pioche une carte avant même que nous recevions les deux notres.
+Mais alors est-il possible de faire varier sa stratégie suivant la valeur qu'il tire ?
+Par exemple, s'il pioche une carte en dessous ou égale à 5, il sera obligé de piocher deux autres cartes car le maximum qu'il peut tirer est 11 (un As) et 5 + 11 = 16 qui est inferieur à 17.
+En revanche, s'il tire une carte supérieur ou égale à 6, il peut gagner seulement avec une seule carte de plus.
+## Etape 1 : Le jeu
+
+### Règle du jeu : 
+
+#### But du jeu
+
+Après avoir reçu deux cartes, le joueur tire des cartes pour s’approcher de la valeur 21 sans la   dépasser. Le but du joueur est de battre le croupier en obtenant un total de points supérieur à   celui-ci ou en voyant ce dernier dépasser 21. Chaque joueur joue contre le croupier, qui représente  
+la banque, ou le casino, et non contre les autres joueurs.
+
+#### Valeurs des cartes au blackjack
+
+Chaque carte numérotée de 2 à 10 a sa valeur nominale (égale au numéro sur la carte)  
+Les valets, les dames et les rois (les figures), ont une valeur de 10 points  
+L’As vaut 1 point ou 11 points, au choix du joueur  
+
+
+Pour plus de détail : https://www.guide-blackjack.com/regles-du-black-jack.html
+
+
+## Etape 2 : La recherche
+
+### Partie naive : 
+Pour cette partie, j'ai codée une méthode qui permet de jouer un tour de manière naif, (i.e. le bot ne s'arrête que quand la somme des cartes dépasse ou est égale à la valeur donnée).
+
+Grâce à ça, j'ai pu établir une moyenne du pourcentage de victoire et de défaite sur 1 000 000 de partie.
+
+J'ai ensuite rempli un tableau avec ces valeurs et de là, est sorti ce graphique : 
+![Graphique courbes](courbe_version_naive.jpg)
+
+On peut voir que la probabilité d'avoir une partie gagnante ou ex-aequo est la plus élevée lorsqu'on arrête de tirer quand la somme de nos cartes est au moins de 17.  
+Cela appuie le fait que dans les casinos les IA qui servent de croupier s'arrêtent de tirer dès qu'elles ont un total de 17 points au moins.
+
+### Partie complète : 
+Danc cette partie, on prend en compte la première carte piochée par le croupier.
+
+## Etape 3 : La partie graphique
+Je développerai peut-être cette partie plus tard, elle n'était pas le coeur de l'interrogation
+
+# Installation : 
+#### Jouer depuis un terminal : 
+```bash
+git clone git@github.com:Ne0re0/BlackJack.git
+cd BlackJack
+/bin/python /home/neo/Projets/BlackJack/blackjack.py
+```
---
+++ blackjack.py
@@ -0,0 +1,287 @@
+from random import *
+
+# Constructeur de la classe joueur
+# Le joueur est déterminée par une instance de cette classe
+# Le croupier est simplement un joueur un peu spécial (+ Attribut ValeurApresLaquelleOnNePiochePlus)
+class Joueur():
+    def __init__(self):
+        self.cartes = [] # Les cartes piochées au fur et à mesure
+        self.nom = "" 
+        self.valeurApresLaquelleOnNePiochePlus = 0 #le nom est un peu long XD
+        self.montantJouable = 0
+
+    def ajouterCarte(self,carte):
+        """Ajoute une carte à la main du joueur"""
+        self.cartes.append(carte)
+
+    def getCartes(self):
+        """Renvoie la liste des cartes présentes dans la main de joueur"""
+        return self.cartes
+
+    def setNom(self, nom = ""):
+        """Modifie le nom de l'instance"""
+        if nom != "": 
+            self.nom = nom
+        else : 
+            self.nom = str(input("Quel est votre nom ? : "))
+
+    def getNom(self):
+        """Renvoie le nom de l'instance"""
+        return self.nom
+
+    def setValeurApresLaquelleOnNePiochePlus(self, entier):
+        """Uniquement utilisé par le croupier et par les bots
+        Etablie une valeur telle que lorsque la somme des cartes de la main du joueur dépassent cette valeur,
+        le bot arrête de piocher"""
+        self.valeurApresLaquelleOnNePiochePlus = entier
+
+    def getValeurApresLaquelleOnNePiochePlus(self):
+        """Renvoie la valeur définie juste au dessus"""
+        return self.valeurApresLaquelleOnNePiochePlus
+
+    def getMontantJouable(self):
+        """Le montant jouable correpond a l'argent sur le compte du joueur"""
+        return self.montantJouable
+    
+    def setMontantJouable(self, valeur):
+        """Modifie la valeur du montant jouable"""
+        self.montantJouable = valeur
+
+    def soustraireMontantJouable(self,valeur):
+        """Soustrait une valeur donnée au montant jouable
+        Cette soustraction est effectuée lors du pari"""
+        self.montantJouable -= valeur
+
+    def ajouterMontantJouable(self,valeur):
+        """Ajoute une valeur donnée au montant jouable"""
+        self.montantJouable += valeur
+
+    def getSommeCartes(self):
+        """Renvoie la somme des cartesprésentes dans la main du joueur
+        A savoir que Vallet,Dame,Roi et As valent 10
+        L'As peut aussi valoir seulement 1 si et seulement si la somme dépasse 21"""
+        somme = 0
+        quantiteAs = 0
+        for carte in self.cartes :
+            try : 
+                somme += int(carte[:-1])
+            except :
+                if carte[:-1] == "A":
+                    quantiteAs += 1
+                    somme += 11
+                else :
+                    somme += 10
+        while somme > 21 and quantiteAs > 0 :
+            somme -= 9
+            quantiteAs -= 1
+        return somme
+    
+    def resetMain(self):
+        """Supprime toutes les cartes de la main du joueur"""
+        self.cartes = []
+
+    def afficherLaMain(self):
+        """Tout est dans le nom"""
+        if self.getNom() == "Croupier":
+            print("\t\tMain du croupier : ")
+        else :
+            print(f"\t\tMain de {self.getNom()} :")
+        print(f"\t\t", end="   ")
+        for carte in self.cartes : 
+            print(carte, end = " ")
+        print("(" + str(self.getSommeCartes()) + ")")
+
+# Constructeur du jeu en lui même
+class BlackJack():
+    def __init__(self):
+        self.cartesRestantes = [] # le paquet de carte
+        self.sommeMaximalePossible = 21 # La somme à ne pas dépasser
+        self.resetJeu() # Paquet de carte neuf
+
+    def piocher(self):
+        """Renvoie une carte au hasard dans le paquet de jeu et la supprime de celui-ci"""
+        cartePioche = choice(self.cartesRestantes)
+        self.cartesRestantes.remove(cartePioche)
+        return cartePioche
+
+    def resetJeu(self):
+        """Le paquet de jeu redevient tout neuf"""
+        self.cartesRestantes = ['2♥', '2♦', '2♣', '2♠', '3♥', '3♦', '3♣', '3♠', '4♥', '4♦', '4♣', '4♠', '5♥', '5♦', '5♣', '5♠', '6♥', '6♦', '6♣', '6♠', '7♥', '7♦', '7♣', '7♠', '8♥', '8♦', '8♣', '8♠', '9♥', '9♦', '9♣', '9♠', '10♥', '10♦', '10♣', '10♠', 'V♥', 'V♦', 'V♣', 'V♠', 'D♥', 'D♦', 'D♣', 'D♠', 'R♥', 'R♦', 'R♣', 'R♠', 'A♥', 'A♦', 'A♣', 'A♠']
+
+    def tour(self, croupier, joueur, montantParie):
+        """Implémente un tour de jeu"""
+        # Creation des variables et affichage de départ
+        # Debut du jeu
+        # Le croupier pioche toujours une carte en premier
+        self.resetJeu()
+        croupier.ajouterCarte(self.piocher())
+        croupier.afficherLaMain()
+        # Ensuite c'est au tour du joueur
+        joueur.ajouterCarte(self.piocher())
+        joueur.ajouterCarte(self.piocher())
+        joueur.afficherLaMain()
+        
+        # On demande au joueur s'il veut continuer de piocher ou arreter
+        jeuTermine = False 
+        while not jeuTermine : 
+            STR_INPUT_PIOCHE_STOP = "\tChoix du joueur : (1:pioche | 2:stop) :"
+            volonteDuJoueur = input(STR_INPUT_PIOCHE_STOP)
+            while volonteDuJoueur not in ["1","2"]:
+                print("Erreur, veuillez retaper : ")
+                volonteDuJoueur = input(STR_INPUT_PIOCHE_STOP)
+            # Cas où il souhaite arreter
+            if volonteDuJoueur == "2" :
+                break
+            # Cas où il souhaite piocher
+            elif volonteDuJoueur == "1" : 
+                joueur.ajouterCarte(self.piocher())
+                joueur.afficherLaMain()
+            # A la fin de chaque tour on vérifie que la somme des cartes n'a pas dépassée le maximum
+            if joueur.getSommeCartes() > self.sommeMaximalePossible : 
+                print("\tTOUR PERDU")
+                return 0
+            if joueur.getSommeCartes() == 21:
+                print("\tBLACKJACK !!")
+                break
+        if joueur.getSommeCartes() <= self.sommeMaximalePossible :
+            while croupier.getSommeCartes() < croupier.getValeurApresLaquelleOnNePiochePlus():
+                croupier.ajouterCarte(self.piocher()) 
+                croupier.afficherLaMain()
+            if croupier.getSommeCartes() > self.sommeMaximalePossible :
+                print("\tTOUR GAGNE")
+                return montantParie*2
+            elif croupier.getSommeCartes() > joueur.getSommeCartes():
+                print("\tTOUR PERDU")
+                return 0
+            elif croupier.getSommeCartes() == joueur.getSommeCartes():
+                print("\tEGALITE")
+                return montantParie
+            elif croupier.getSommeCartes() < joueur.getSommeCartes():
+                print("\tTOUR GAGNE")
+                return 2*montantParie
+            print("ERREUR TOUR()")
+            return 0
+        
+
+    def jouer(self):
+        """Implémente une partie complète du jeu jusqu'à ce que le compte soit vide ou que le joueur veuille arreter"""
+        croupier = Joueur()
+        croupier.setNom("Croupier")
+        croupier.setValeurApresLaquelleOnNePiochePlus(17)
+        joueur = Joueur()
+        joueur.setNom()
+
+        # Definition du montant jouable
+        AFFICHAGE_INPUT_MONTANT_JOUABLE = "Entrez le montant maximal que vous pourrez jouer (en €) : "
+        montantJouable = input(AFFICHAGE_INPUT_MONTANT_JOUABLE)
+        try :
+            montantJouable = int(montantJouable)
+        except:
+            pass
+        while type(montantJouable) != int or montantJouable <= 0:
+            print("Erreur, Veuillez ressaisir")
+            montantJouable = input(AFFICHAGE_INPUT_MONTANT_JOUABLE)
+            try :
+                montantJouable = int(montantJouable)
+            except :
+                pass
+        joueur.setMontantJouable(montantJouable)
+
+        # Lancement du corps du jeu
+        partieEnCours = "O"
+        while partieEnCours.upper() == "O":
+            print()
+            print("***")
+            self.resetJeu()
+            croupier.resetMain()
+            joueur.resetMain()
+            AFFICHAGE_INPUT_MONTANT_PARI = "\tRentrez le montant que vous voulez parier (somme maximal : " + str(joueur.getMontantJouable())+ ") : "
+
+            montantParie = input(AFFICHAGE_INPUT_MONTANT_PARI)
+            try :
+                montantParie = int(montantParie)
+            except:
+                pass
+
+            while type(montantParie) !=  int or  montantParie <= 0 or montantParie > joueur.getMontantJouable():
+                print("\tErreur, veuillez ressaisir")
+                montantParie = input(AFFICHAGE_INPUT_MONTANT_PARI)
+                try :
+                    montantParie = int(montantParie)
+                except:
+                    pass
+
+            # lancement de la partie
+            joueur.soustraireMontantJouable(montantParie)
+            resultatDuTour = self.tour(croupier, joueur, montantParie)
+            joueur.ajouterMontantJouable(resultatDuTour)
+            print()
+            print(f"NOUVEAU SOLDE : {joueur.getMontantJouable()}€")
+            if joueur.getMontantJouable() == 0:
+                print(f"PLUS AUCUN MONTANT JOUABLE : Arrêt de la partie")
+                return
+            # Demande si le joueur souhaite continuer
+            AFFICHAGE_INPUT_CONTINUER_PARTIE = "\tSouhaitez vous continuer ? (o/n) : "
+            partieEnCours = input(AFFICHAGE_INPUT_CONTINUER_PARTIE)
+            while partieEnCours.upper() not in ["O","N",""]:
+                print(f"\tErreur, veuillez ressaisir")
+                partieEnCours = input(AFFICHAGE_INPUT_CONTINUER_PARTIE)
+            if partieEnCours == "":
+                partieEnCours = "O"
+
+
+    def tourIAcontreIA(self, croupier, bot, montantParie):
+        croupier.ajouterCarte(self.piocher())
+        while bot.getSommeCartes() < bot.getValeurApresLaquelleOnNePiochePlus():
+            bot.ajouterCarte(self.piocher())
+        while croupier.getSommeCartes() < croupier.getValeurApresLaquelleOnNePiochePlus():
+            croupier.ajouterCarte(self.piocher())
+        if bot.getSommeCartes() > 21 : 
+            return 0
+        elif croupier.getSommeCartes() > 21 :
+            return 2*montantParie
+        elif croupier.getSommeCartes() == bot.getSommeCartes():
+            return montantParie
+        elif croupier.getSommeCartes() > bot.getSommeCartes():
+            return 0
+        elif croupier.getSommeCartes() < bot.getSommeCartes():
+            return 2*montantParie
+
+    def etudeStrategieUneValeurMax(self,aPartirDeCeNombreOnArreteDePiocher):
+        pourcentages = {"P":0,"E":0,"G":0}
+        croupier = Joueur()
+        croupier.setNom("Croupier")
+        croupier.setValeurApresLaquelleOnNePiochePlus(17)
+
+        bot = Joueur()
+        bot.setNom("Bot")
+        bot.setValeurApresLaquelleOnNePiochePlus(aPartirDeCeNombreOnArreteDePiocher) # Oui oui il y a seulement deux appels
+        bot.setMontantJouable(1000000)
+
+        for k in range(1000000):
+            self.resetJeu()
+            bot.resetMain()
+            croupier.resetMain()
+            montantParie = 1
+            bot.soustraireMontantJouable(montantParie)
+            resultatDuTour = self.tourIAcontreIA(croupier, bot, montantParie)
+            if resultatDuTour == 0 :
+                pourcentages["P"] += 1
+            elif resultatDuTour == montantParie :
+                pourcentages["E"] += 1
+            else : 
+                pourcentages["G"] += 1
+            bot.ajouterMontantJouable(resultatDuTour)
+        print(f"Somme à la fin : {bot.getMontantJouable()}")
+        return pourcentages
+
+    def etudeStrategie(self):
+        print("Le croupier s'arrête de piocher quand il a au moins 17 points")
+        for valeurMax in range(4,21):
+            print(f"On s'arrete de piocher lorsqu'on a au moins {valeurMax} points")
+            print(self.etudeStrategieUneValeurMax(valeurMax))
+
+
+if __name__ == '__main__':
+    b = BlackJack()
+    print(b.etudeStrategieUneValeurMax(21))
